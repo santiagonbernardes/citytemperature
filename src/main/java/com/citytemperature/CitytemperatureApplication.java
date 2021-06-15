f8ffbf29d53dc5128012ea7f6c0856ea0ec8dcad
Santiago Bernardes <santiagobernardes@gmail.com>
2021-06-15 20:44:00 -0300

    Imports files from Spring Initializr

---
+++ src/main/java/com/citytemperature/CitytemperatureApplication.java
@@ -0,0 +1,13 @@
+package com.citytemperature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitytemperatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitytemperatureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/citytemperature/CitytemperatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citytemperature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitytemperatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
